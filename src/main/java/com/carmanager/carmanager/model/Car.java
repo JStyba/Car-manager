e945f54304407d5737601beee03b6be89d143d3d
JAREK <jarek.styba@gmail.com>
2018-04-21 11:17:21 +0200

    my car model, przemek controller appuser

--- src/main/java/com/carmanager/carmanager/model/Car.java
+++ src/main/java/com/carmanager/carmanager/model/Car.java
@@ -1,4 +1,38 @@
 package com.carmanager.carmanager.model;
 
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@Entity
+@NoArgsConstructor
 public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String make;
+    private String model;
+    private int dateProduced;
+    private String vin;
+    private Double engineCapacity;
+    private int numberOfSeats;
+    private String registrationNumber;
+
+
+    @OneToMany (mappedBy = "repair")
+    private List<Repairs> repairsList;
+    @ManyToOne
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "appuser")
+    private AppUser appUser;
+
+
+
+
+
 }
