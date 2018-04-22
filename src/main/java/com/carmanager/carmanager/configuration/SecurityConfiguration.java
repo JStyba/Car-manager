3524ab740962f7031e3a74a1cbdd397313dec069
JAREK <jarek.styba@gmail.com>
2018-04-22 11:30:00 +0200

    expenses controller postman ready

--- src/main/java/com/carmanager/carmanager/configuration/SecurityConfiguration.java
+++ src/main/java/com/carmanager/carmanager/configuration/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.carmanager.carmanager.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,5 +17,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .authorizeRequests()
+                .anyRequest().permitAll();
+    }
 }
---
+++ src/main/java/com/carmanager/carmanager/controller/ExpensesController.java
@@ -0,0 +1,31 @@
+package com.carmanager.carmanager.controller;
+
+import com.carmanager.carmanager.model.Expenses;
+import com.carmanager.carmanager.service.ExpenseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@CrossOrigin
+@RestController
+@RequestMapping ("/expenses/")
+public class ExpensesController {
+
+    @Autowired
+    private ExpenseService expenseService;
+
+
+    @RequestMapping(path = "/listexpenses", method = RequestMethod.GET)
+    public List<Expenses> listExpenses() {
+        List<Expenses> expensesList = expenseService.getAll().stream()
+                .map(expenses -> new Expenses(expenses.getName()))
+                .collect(Collectors.toList());
+        return expensesList;
+    }
+}
