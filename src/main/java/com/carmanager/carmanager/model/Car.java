8bfe7fc7b95d42b888a18d002971bd37796e6342
JAREK <jarek.styba@gmail.com>
2018-05-02 20:25:50 +0200

    everything seems to be working

--- src/main/java/com/carmanager/carmanager/model/Car.java
+++ src/main/java/com/carmanager/carmanager/model/Car.java
@@ -2,18 +2,22 @@ package com.carmanager.carmanager.model;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.util.List;
 import java.util.Set;
 
-@Data
+
 @Entity
-@NoArgsConstructor
+
 @AllArgsConstructor
+
 public class Car {
 
 
@@ -38,7 +42,8 @@ public class Car {
     @JsonIgnore
     private AppUser appUser;
 
-    @OneToMany(cascade = CascadeType.PERSIST, mappedBy = "car")
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "car")
+    @JsonManagedReference
     private Set<Expenses> expensesSet;
 
     @OneToMany(cascade = CascadeType.PERSIST, mappedBy = "car")
@@ -46,6 +51,9 @@ public class Car {
 
     @OneToMany(cascade = CascadeType.PERSIST, mappedBy = "car")
     private Set<Repairs> repairsSet;
+
+    public Car() {
+    }
 
     public Car(String fuelType, String make, String model, int dateProduced, String vin, Double engineCapacity, int numberOfSeats, String registrationNumber) {
         this.fuelType = fuelType;
@@ -70,5 +78,117 @@ public class Car {
         this.registrationNumber = registrationNumber;
 
 
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getDateProduced() {
+        return dateProduced;
+    }
+
+    public void setDateProduced(int dateProduced) {
+        this.dateProduced = dateProduced;
+    }
+
+    public String getVin() {
+        return vin;
+    }
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
+
+    public Double getEngineCapacity() {
+        return engineCapacity;
+    }
+
+    public void setEngineCapacity(Double engineCapacity) {
+        this.engineCapacity = engineCapacity;
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public void setNumberOfSeats(int numberOfSeats) {
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public List<Repairs> getRepairsList() {
+        return repairsList;
+    }
+
+    public void setRepairsList(List<Repairs> repairsList) {
+        this.repairsList = repairsList;
+    }
+
+    public AppUser getAppUser() {
+        return appUser;
+    }
+
+    public void setAppUser(AppUser appUser) {
+        this.appUser = appUser;
+    }
+
+    public Set<Expenses> getExpensesSet() {
+        return expensesSet;
+    }
+
+    public void setExpensesSet(Set<Expenses> expensesSet) {
+        this.expensesSet = expensesSet;
+    }
+
+    public Set<Fees> getFeesSet() {
+        return feesSet;
+    }
+
+    public void setFeesSet(Set<Fees> feesSet) {
+        this.feesSet = feesSet;
+    }
+
+    public Set<Repairs> getRepairsSet() {
+        return repairsSet;
+    }
+
+    public void setRepairsSet(Set<Repairs> repairsSet) {
+        this.repairsSet = repairsSet;
     }
 }
--- src/main/java/com/carmanager/carmanager/model/Expenses.java
+++ src/main/java/com/carmanager/carmanager/model/Expenses.java
@@ -1,32 +1,38 @@
 package com.carmanager.carmanager.model;
 
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Set;
 
-@Data
-@Entity
-@NoArgsConstructor
 
-public class Expenses {
+@Entity
+
+
+public class Expenses{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @ManyToOne
+    @JsonBackReference
     private Car car;
 
     private String name;
     private LocalDate expenseDate;
     private String expenseCost;
     private String expenseDescription;
-    @Enumerated (EnumType.STRING)
-    private ExpensesEnum expensesEnum;
+
+    public Expenses() {
+    }
 
     public Expenses(String name) {
         this.name = name;
@@ -44,6 +50,54 @@ public class Expenses {
         this.name = name;
         this.expenseDate = expenseDate;
         this.expenseCost = expenseCost;
+        this.expenseDescription = expenseDescription;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getExpenseDate() {
+        return expenseDate;
+    }
+
+    public void setExpenseDate(LocalDate expenseDate) {
+        this.expenseDate = expenseDate;
+    }
+
+    public String getExpenseCost() {
+        return expenseCost;
+    }
+
+    public void setExpenseCost(String expenseCost) {
+        this.expenseCost = expenseCost;
+    }
+
+    public String getExpenseDescription() {
+        return expenseDescription;
+    }
+
+    public void setExpenseDescription(String expenseDescription) {
         this.expenseDescription = expenseDescription;
     }
 }
